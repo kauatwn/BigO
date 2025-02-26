577e650b2b7731cbe1c799072aac513daaf0c004
kauatwn <kaualopescontato@gmail.com>
2025-02-26 15:41:34 -0300

    Adiciona exemplos de operações O(1)

--- src/Constant/Program.cs
+++ src/Constant/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int[] array = [1, 2, 3, 4, 5];
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"O(1): {array[0]}");
+
+// É O(1) quando:
+
+// Não tem laços de repetição
+// Acessos direto a elementos
+// Operações mateḿaticas simples
+// Uso de estruturas de dados eficientes (métodos de busca, inserção, remoção são geralmente O(1))
+
+var ages = new Dictionary<string, int>
+{
+    { "Alice", 25 },
+    { "Bob", 30 },
+    { "Josh", 35 }
+};
+
+// Adicionando um novo elemento com atribuição direta
+ages["Diana"] = 28; // O(1)
+Console.WriteLine($"Idade da Diana: {ages["Diana"]}");
+
+// Acessando um elemento com método Add()
+ages.Add("Charlie", 40); // O(1)
+Console.WriteLine($"Idade do Charlie: {ages["Charlie"]}");
+
+// Removendo um elemento
+ages.Remove("Bob"); // O(1)
+Console.WriteLine("Bob foi removido do dicionário.");
+
+// Verificando se um elemento existe
+if (ages.ContainsKey("Alice")) // O(1)
+{
+    Console.WriteLine("Alice está no dicionário.");
+}
+
+// O(1) é a complexidade de tempo mais eficiente que existe. Significa que o algoritmo é constante, independentemente
+// do tamanho da entrada.
